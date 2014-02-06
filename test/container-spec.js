35a98d74b8cdd58ea0d447c23371b2f98e9d961e
Tyler Garlick <tjgarlick@gmail.com>
2014-02-06 15:39:57 -0500

    some additional fixes

--- test/container-spec.js
+++ test/container-spec.js
@@ -16,4 +16,22 @@ describe('Container', function () {
 
 
   });
+
+  describe('.register(model, schama, options)', function () {
+
+    var basicSchema = ignitor.Types.Schema({
+      name: { type: 'string' },
+      age: { type: 'number'}
+    });
+
+    it('should register a model', function () {
+
+      expect(ignitor.__.container.all().length).to.be(0);
+      ignitor.__.container.register('basic', basicSchema);
+
+
+    });
+
+
+  });
 });
--- test/helpers-spec.js
+++ test/helpers-spec.js
@@ -12,7 +12,6 @@ describe('helper', function () {
         expect(helper.db.collections.create).to.be.ok();
       });
     });
-
   });
 
   describe('objects', function () {
