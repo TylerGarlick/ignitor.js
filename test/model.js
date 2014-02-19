99a000989ab804dc7c2eb3f2a47bc14cc1578a27
Tyler Garlick <tjgarlick@gmail.com>
2014-02-18 20:33:32 -0500

    refactor

--- test/model.js
+++ test/model.js
@@ -1,51 +1,49 @@
 'use strict';
+
 var expect = require('expect.js')
-  , ignitor = require('../lib')
-  , Types = ignitor.Types
-  , Schema = Types.Schema
-  , Model = Types.Model
+  , Model = require('../lib/model')
   ;
 
 
 describe('Model', function () {
 
-  describe('#new', function () {
-    describe("new Model(schema) should register model successfully", function () {
-      it('should instantiate with a Schema object', function () {
-        var schema = new Schema({
-          name: {
-            type: 'string',
-            default: "Tyler Garlick"
-          }
-        });
-        expect(schema).to.be.ok();
+  describe('constructor', function () {
 
-        var model = new Model(schema);
-        expect(model).to.be.ok();
-        expect(model._schema).to.be.equal(schema);
-        expect(model._isNew).to.be.ok();
-      });
+    it('the collection property should be stored', function () {
+      var model = new Model(null, { collection: 'Model'});
+      expect(model).to.be.ok();
+      expect(model._collection).to.be.equal('model');
+
     });
 
-    describe("new Model(instance) should register add values to model successfully", function () {
-      it('should instantiate with instance values', function () {
-        var instance = {
-          _key: 1234,
-          _id: 1234,
-          _rev: 1234,
-          name: "Captain America",
-          email: "capt@america.com",
-          active: true
-        };
-        var model = new Model(instance);
-        expect(model).to.be.ok();
-        expect(model._isNew).to.not.be.ok();
-        expect(model.name).to.equal(instance.name);
-        expect(model.email).to.equal(instance.email);
-        expect(model.active).to.equal(instance.active);
+    it('should store the schema in the _schema property', function () {
+      var model = new Model(null, {
+        schema: {
+          name: { type: 'string', required: true },
+          age: { type: 'numeric', default: 0},
+          active: { type: 'boolean', default: true }
+        }
       });
+
+      expect(model).to.be.ok();
+      expect(model._schema).to.be.ok();
+
+      expect(model._schema.name.type).to.be.equal('string');
+      expect(model._schema.name.required).to.be.ok();
+
+      expect(model._schema.age.type).to.be.equal('numeric');
+      expect(model._schema.age.default).to.be.equal(0);
+
+      expect(model._schema.active.type).to.be.equal('boolean');
+      expect(model._schema.active.default).to.be.equal(true);
+
     });
 
+    it('should combine the model and the instance', function () {
+
+    });
 
   });
+
 });
+
