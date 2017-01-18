e07165f441227106219aaacd6eedee92fab8f83e
Tyler Garlick <tjgarlick@gmail.com>
2017-01-18 16:27:47 -0700

    Added the #executeWithFirst()
    - executeWithFirst retrieves all, then takes the first results and returns a single document

--- src/query.js
+++ src/query.js
@@ -1,14 +1,25 @@
 export default (db) => {
-
+  
   return {
+    
     async execute(query, params) {
       return await db.query(query, params);
     },
-
+    
     async executeAll(query, params) {
       const cursor = await this.execute(query, params);
       return await cursor.all();
+    },
+    
+    async executeWithFirst(query, params) {
+      const results = await this.executeAll(query, params);
+      
+      return results.reduce((prev, current) => {
+        return current;
+      }, []);
     }
+    
+    
   };
-
+  
 };
